e79a9b40c6eb06c5147ae260cbe8bd982196c7ea
Possed <r0xana.pirvu@yahoo.com>
2018-06-18 12:11:36 +0100

    account class gets initialized with an empty transaction - tested&passes

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,5 +6,9 @@ describe Account do
     it 'initializes a new account with balance = 0' do
       expect(subject.balance).to equal(0)
     end
+    it 'initializes a new account with an empty transaction' do
+      expect(subject.transaction).to be_empty
+    end
   end
+
 end
