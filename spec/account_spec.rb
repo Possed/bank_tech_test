5d6641073b3ec823819fc76db958f27676d7d490
Possed <r0xana.pirvu@yahoo.com>
2018-06-18 11:54:23 +0100

    first test passed - account class gets initialized with a balance equal to 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,10 @@
 require 'account'
 
 describe Account do
-  
+
+  describe '#initialize' do
+    it 'initializes a new account with balance = 0' do
+      expect(subject.balance).to equal(0)
+    end
+  end
 end
