fbcd79335e4d804e73d6edcbf8cf3656a8f4fccd
Possed <r0xana.pirvu@yahoo.com>
2018-06-19 11:58:39 +0100

    created a print statement method that prints a header and iterated over the statement arry and prints the transactions - test passed

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,6 +7,12 @@ class Statement
 
   def add_to_statement(transaction)
     @statement << transaction;
+  end
 
+  def print_statement
+    puts "   date   ||  credit  ||  debit  ||  balance  ";
+    @statement.each {|transaction1|
+    puts " #{transaction1[:date]}||#{transaction1[:credit]}||#{transaction1[:debit]}||#{transaction1[:balance]}"
+    }
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,6 +1,7 @@
 require 'statement'
 
 describe Statement do
+
   describe '#initialize' do
     it 'initializes an intance of the class with an empy array' do
       expect(subject.statement).to eq([])
@@ -14,7 +15,11 @@ describe Statement do
     end
   end
 
-  # describe 'print_statement' do
-  #   it 'prints the account statement'
-  # end
+  describe '#print_statement' do
+    it 'prints the account statement' do
+      subject.add_to_statement({date: '18/06/2018', credit: 5, balance: 10})
+      expect{subject.print_statement}.to output("   date   ||  credit  ||  debit  ||  balance  \n #{subject.statement[0][:date]}||#{subject.statement[0][:credit]}||#{subject.statement[0][:debit]}||#{subject.statement[0][:balance]}\n").to_stdout
+    end
+  end
+
 end
