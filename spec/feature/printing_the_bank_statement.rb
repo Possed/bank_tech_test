60bde15405f2285c6f6ca6eb72955777858899a6
Possed <r0xana.pirvu@yahoo.com>
2018-06-20 16:39:25 +0100

    updated feature test with the exact acceptance criteria - test passed

--- spec/feature/printing_the_bank_statement.rb
+++ spec/feature/printing_the_bank_statement.rb
@@ -4,14 +4,14 @@ describe 'Test - print the bank statement' do
 
   it 'Make 2 deposits, 1 withdrawal and print the statement' do
     account = Account.new
-    account.deposit(1000)
-    account.deposit(2000)
-    account.withdrawal(500)
+    account.deposit(1000, "10/01/2012")
+    account.deposit(2000, "13/01/2012")
+    account.withdrawal(500, "14/01/2012")
 
     expect{account.acc_statement.print_statement}.to output("    date   ||  credit  ||  debit  ||  balance  
- 20/06/2018||             ||  500  ||  2500
- 20/06/2018||    2000   ||          ||  3000
- 20/06/2018||    1000   ||          ||  1000
+ 14/01/2012||             ||  500.00  ||  2500.00
+ 13/01/2012||    2000.00   ||          ||  3000.00
+ 10/01/2012||    1000.00   ||          ||  1000.00
 ").to_stdout
 
   end
