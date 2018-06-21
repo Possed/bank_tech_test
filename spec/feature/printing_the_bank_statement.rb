dcf928c6a9bc3e578f9c50a8276a72b1ef101ca0
Possed <r0xana.pirvu@yahoo.com>
2018-06-21 14:10:26 +0100

    added 2 spaces in the output text to pass the test

--- spec/feature/printing_the_bank_statement.rb
+++ spec/feature/printing_the_bank_statement.rb
@@ -5,7 +5,7 @@ describe 'Test - print the bank statement' do
     account.deposit(1000, "10/01/2012")
     account.deposit(2000, "13/01/2012")
     account.withdrawal(500, "14/01/2012")
-    expect { account.acc_statement.print_statement }.to output("    date   ||  credit  ||  debit  ||  balance
+    expect { account.acc_statement.print_statement }.to output("    date   ||  credit  ||  debit  ||  balance  
  14/01/2012||             ||  500.00  ||  2500.00
  13/01/2012||    2000.00   ||          ||  3000.00
  10/01/2012||    1000.00   ||          ||  1000.00
